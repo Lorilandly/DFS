3d5a861b909acb5b8c1dd4fbb0f076f33bb91042
Leo Chen <minghaoc@andrew.cmu.edu>
2024-03-27 03:59:25 -0700

    implement storage server initialization Co-authored-by: Haoan Li <haoanliF2023@users.noreply.github.com>

--- storage/src/main.rs
+++ storage/src/main.rs
@@ -1,6 +1,6 @@
 mod handlers;
 mod routes;
-use std::fs;
+mod storage;
 use std::{future::IntoFuture, path::Path};
 
 #[tokio::main]
@@ -25,49 +25,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("Failed to parse registration port");
     let root_storage_dir = Path::new(&args[4]);
 
+    let storage = storage::Storage::new(root_storage_dir);
+    storage
+        .initialize_storage(
+            client_port,
+            command_port,
+            registration_port,
+            root_storage_dir,
+        )
+        .await?;
+
     let client_app = routes::client_routes();
     let client_listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", client_port)).await?;
 
     let command_app = routes::command_routes();
     let command_listener =
         tokio::net::TcpListener::bind(format!("0.0.0.0:{}", command_port)).await?;
-
-    let client = reqwest::blocking::Client::new();
-    // {
-    //     "storage_ip": "localhost",
-    //     "client_port": 1111,
-    //     "command_port": 2222,
-    //     "files": [
-    //         "/fileA",
-    //         "/path/to/fileA",
-    //         "/path/to/fileB",
-    //         "/path/to/another/fileA"
-    //     ]
-    // }
-
-    let paths = fs::read_dir(root_storage_dir).unwrap();
-    let mut files = vec![];
-    for path in paths {
-        let path = path.unwrap().path();
-        if path.is_file() {
-            files.push(path.to_str().unwrap().to_string());
-        }
-    }
-    let body = format!(
-        "{{\"storage_ip\": \"localhost\", \"client_port\": {}, \"command_port\": {}, \"files\": {:?}}}",
-        client_port, command_port, files
-    );
-
-    println!("\nn\n\n\n\nbody: {}", body);
-
-    let _res = client
-        .post(format!("http://localhost:{}/register", registration_port))
-        .body(format!(
-            "{{\"storage_ip\": \"localhost\", \"client_port\": {}, \"command_port\": {}, \"files\": {:?}}}",
-            client_port, command_port, files
-        ))
-        .send()
-        .unwrap();
 
     let _ = tokio::join!(
         axum::serve(client_listener, client_app).into_future(),
--- storage/src/storage.rs
+++ storage/src/storage.rs
@@ -1,18 +1,90 @@
-use std::collections::BTreeMap;
-
+use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
 pub struct Storage {
-    files: Vec<String>,
+    root_storage_dir: String,
 }
 
-impl Default for Storage {
-    fn default() -> Self {
-        let files = vec![];
-        Self { files }
-    }
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct RegisterRequest {
+    pub storage_ip: String,
+    pub client_port: u16,
+    pub command_port: u16,
+    pub files: Vec<PathBuf>,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct RegisterResponse {
+    pub files: Vec<PathBuf>,
 }
 
 impl Storage {
-    pub fn insert(&mut self, file: String) {
-        self.files.push(file);
+    pub fn new(root: &Path) -> Self {
+        Self {
+            root_storage_dir: root.to_str().unwrap().to_string(),
+        }
+    }
+
+    pub async fn initialize_storage(
+        &self,
+        client_port: u16,
+        command_port: u16,
+        registration_port: u16,
+        root_storage_dir: &Path,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let client = reqwest::blocking::Client::new();
+
+        let files = self
+            .get_all_files_recursive(root_storage_dir)
+            .expect("Error when getting files in storage directory");
+        println!("files: {:?}", files);
+        let res = client
+            .post(format!("http://localhost:{}/register", registration_port))
+            .json(&RegisterRequest {
+                storage_ip: "localhost".to_string(),
+                client_port,
+                command_port,
+                files,
+            })
+            .send()?;
+
+        let response = res.json::<RegisterResponse>()?;
+        println!("response: {:?}", response);
+        for file in response.files {
+            let path_to_delete = root_storage_dir.join(file.strip_prefix("/")?);
+            println!("deleting: {:?}", path_to_delete);
+            fs::remove_file(&path_to_delete)?;
+            if let Some(parent) = path_to_delete.parent() {
+                self.remove_dir_recursive(parent)?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn remove_dir_recursive(&self, path: &Path) -> Result<(), std::io::Error> {
+        if self.is_dir_empty(path)? {
+            fs::remove_dir(path)?;
+            if let Some(parent) = path.parent() {
+                self.remove_dir_recursive(parent)?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn is_dir_empty(&self, path: &Path) -> Result<bool, std::io::Error> {
+        Ok(fs::read_dir(path)?.next().is_none())
+    }
+
+    pub fn get_all_files_recursive(&self, path: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
+        let mut files = vec![];
+        for entry in fs::read_dir(path)? {
+            let path = entry?.path();
+            if path.is_dir() {
+                files.append(&mut self.get_all_files_recursive(&path)?)
+            } else {
+                files.push(Path::new("/").join(path.strip_prefix(&self.root_storage_dir).unwrap()))
+            }
+        }
+        Ok(files)
     }
 }
